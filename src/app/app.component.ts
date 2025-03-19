35810825e6045b81f761cb246162b10b5d79a480
Javier <nhpatt@gmail.com>
2025-03-19 01:37:56 +0100

    nuevo componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TrayectoComponent} from './trayecto/trayecto.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TrayectoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
