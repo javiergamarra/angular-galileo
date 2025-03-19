ca72a4950d926926a246f1b5b53d37025e2c1d04
Javier <nhpatt@gmail.com>
2025-03-19 01:52:23 +0100

    interpolacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {TrayectoComponent} from './trayecto/trayecto.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TrayectoComponent],
+  imports: [TrayectoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
