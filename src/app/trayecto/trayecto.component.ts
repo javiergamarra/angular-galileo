1908c3dfd908918e7afe1763048fd4352ae3ff4a
Javier <nhpatt@gmail.com>
2025-03-19 01:36:54 +0100

    nuevo componente

---
+++ src/app/trayecto/trayecto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trayecto',
+  imports: [],
+  templateUrl: './trayecto.component.html',
+  styleUrl: './trayecto.component.css'
+})
+export class TrayectoComponent {
+
+}
