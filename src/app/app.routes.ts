477e55118eced57427d5515dedf0a2f20dd49de8
Javier <nhpatt@gmail.com>
2025-04-09 14:20:59 +0200

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {TrayectoComponent} from './trayecto/trayecto.component';
+import {OrigenComponent} from './origen/origen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: OrigenComponent},
+  {path: 'trayecto', component: TrayectoComponent},
+];
