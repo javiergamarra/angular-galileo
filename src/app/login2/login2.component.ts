5e071a5f44c73797b2d6c7ff82092487846486fb
Javier <nhpatt@gmail.com>
2025-04-09 14:58:03 +0200

    Computed

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 
 @Component({
   selector: 'app-login2',
@@ -6,12 +6,18 @@ import {Component, signal} from '@angular/core';
   template: `
     <input (keyup)="enviar($event)">
 
+    {{ usuarioMayusculas() }}
   `,
   styleUrl: './login2.component.css'
 })
 export class Login2Component {
 
   usuario = signal('');
+  usuarioMayusculas
+
+  constructor() {
+    this.usuarioMayusculas = computed(() => this.usuario().toUpperCase())
+  }
 
   enviar($event: any) {
     this.usuario.set($event.target.value);
