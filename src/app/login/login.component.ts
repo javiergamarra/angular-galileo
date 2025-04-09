3528bdc24ba63adc74fc493d7aa8759633be5c45
Javier <nhpatt@gmail.com>
2025-04-09 14:51:06 +0200

    two-way databinding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,40 @@
 import {Component} from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   template: `
     <form [formGroup]="form">
       <label>Usuario</label>
       <input name="usuario" formControlName="usuario">
 
+
       <button (click)="enviar()">Login</button>
 
     </form>
+
+    <input [(ngModel)]="banana">
   `,
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
   public form: FormGroup
 
+  banana = 'hola'
+
   constructor(private fb: FormBuilder) {
+
+    setInterval(() => {
+      console.log(this.banana);
+      this.banana = 'hola'
+    }, 1000);
+
+
     this.form = this.fb.group({
       usuario: this.fb.nonNullable.control("", {
         validators: [Validators.required, Validators.email],
